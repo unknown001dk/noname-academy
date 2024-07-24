7a6de4d0e5f19cb7ff72a740bc5cb3185af2b237
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-07-24 22:17:32 +0530

    adding UI routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About, Course, Home, Projects } from './pages/inc.jsx';
+import { Login, Register } from './pages/users/inc.jsx';
+import NotFound from './components/NotFound.jsx';
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path='/course' element={<Course />} />
+        <Route  path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/inc.jsx
@@ -0,0 +1,4 @@
+export { default as Home } from './Home.jsx';
+export { default as About } from './About.jsx';
+export { default as Course } from './Course.jsx';
+export { default as Projects } from './Projects.jsx';

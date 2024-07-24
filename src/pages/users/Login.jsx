198cab22f1bb6354ab1169c45025829d8325aa77
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-07-24 23:38:30 +0530

    adding resgister page UI and pageTitle

--- src/pages/users/Login.jsx
+++ src/pages/users/Login.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import './css/Login.css';
+import { PageTitle } from '../../components/PageTitle';
 
 function Login() {
   return (
-    <section>
-    <div className="form-box">
-      <div className="form-value">
-        <form action="">
-          <h2 className='mb-3'>Login</h2>
-          <div className='form-inputs'>
-            <input className='mb-3' type="text" id="username" name="username" placeholder='Enter username..' required />
+    <>
+    <PageTitle title="Login Page" />
+      <section>
+      <div className="form-box">
+        <div className="form-value">
+          <form action="">
+            <h2 className='mb-3'>Login</h2>
+            <div className='form-inputs'>
+              <input className='mb-3' type="text" id="username" name="username" placeholder='Enter username..' required />
+            </div>
+            <div className='form-inputs'>
+              <input className='mb-3' type="password" id="password" placeholder='Enter your password' name="password" required />
+            </div>
+            <div className='form-btn'>
+              <button>Login</button>
+            </div>
+            <div className='form-links mb-1'>
+              <a href="/forgot-password">Forgot Password?</a>
+              <a href="./register">Create an Account</a>
+            </div>
+          </form>
           </div>
-          <div className='form-inputs'>
-            <input className='mb-3' type="password" id="password" placeholder='Enter your password' name="password" required />
-          </div>
-          <div className='form-btn'>
-            <button>Login</button>
-          </div>
-          <div className='form-links mb-1'>
-            <a href="/forgot-password">Forgot Password?</a>
-            <a href="./register">Create an Account</a>
-          </div>
-        </form>
       </div>
-    </div>
-  </section>
+      </section>
+    </>
   )
 }
 
--- src/pages/users/Register.jsx
+++ src/pages/users/Register.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { PageTitle } from '../../components/PageTitle';
 
 function Register() {
   return (
-    <div>Register</div>
+    <>
+    <PageTitle title='Register page' />
+    <section>
+    <div className="form-box">
+      <div className="form-value">
+        <form action="">
+          <h2 className='mb-3'>Register</h2>
+          <div className='form-inputs'>
+            <input className='mb-3' type="text" id="username" name="username" placeholder='Enter username..' required />
+          </div>
+          <div className='form-inputs'>
+            <input className='mb-3' type="email" id="email" name="email" placeholder='Enter your email' required />
+          </div>
+          <div className='form-inputs'>
+            <input className='mb-3' type="password" id="password" placeholder='Enter your password' name="password" required />
+          </div>
+          <div className='form-btn'>
+            <button>Register</button>
+          </div>
+          <div className='form-links mb-1'>
+            <a href="./login">Already have an Account</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+    </>
   )
 }
 
